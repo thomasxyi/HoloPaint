f723aab491e6858ccc3547f321beb7b7efdc9feb
Radu Cracut <raduc@cs.washington.edu>
2016-05-03 15:37:38 -0700

    added additional color buttons

--- HoloPaint/Assets/Scripts/BrushManager.cs
+++ HoloPaint/Assets/Scripts/BrushManager.cs
@@ -6,12 +6,18 @@ using UnityEngine.VR.WSA.Input;
 
 public class BrushManager : Singleton<BrushManager>
 {
+    private Color color;
 
-    public Color CurrentBrushColor { get; set; }
+    public Color CurrentBrushColor { get { return this.color; }
+                                     set { this.color = value;
+            ((GameObject)Instantiate(Resources.Load("TexturePainter-Instances/SolidBrushEntity"))).GetComponent<Material>().color = value;
+        } }
 
     void Start()
     {
         CurrentBrushColor = Color.red;
     }
+
+    
 
 }     
