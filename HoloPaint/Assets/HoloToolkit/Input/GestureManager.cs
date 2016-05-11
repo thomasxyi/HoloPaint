428d1755c88c3fc889f8cc9443871170ae277184
Thomas Yi <thomasyi@cs.washington.edu>
2016-05-11 16:37:57 -0700

    brush change

--- HoloPaint/Assets/HoloToolkit/Input/GestureManager.cs
+++ HoloPaint/Assets/HoloToolkit/Input/GestureManager.cs
@@ -43,9 +43,6 @@ namespace HoloToolkit.Unity
 
             gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
 
-            // Start looking for gestures.
-            gestureRecognizer.StartCapturingGestures();
-
             // Register for the NavigationStartedEvent with the NavigationRecognizer_NavigationStartedEvent function.
             gestureRecognizer.NavigationStartedEvent += GestureRecognizer_NavigationStartedEvent;
             // Register for the NavigationUpdatedEvent with the NavigationRecognizer_NavigationUpdatedEvent function.
@@ -55,8 +52,8 @@ namespace HoloToolkit.Unity
             // Register for the NavigationCanceledEvent with the NavigationRecognizer_NavigationCanceledEvent function. 
             gestureRecognizer.NavigationCanceledEvent += GestureRecognizer_NavigationCanceledEvent;
 
-
-
+            // Start looking for gestures.
+            gestureRecognizer.StartCapturingGestures();
         }
 
         private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
